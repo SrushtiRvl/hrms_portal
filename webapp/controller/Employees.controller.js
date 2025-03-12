b19d7219a3d6c64f7eb5e8c6cf50aea89a42ee45
Srushti Raval <srushtiraval1205@gmail.com>
2025-03-12 09:09:52 +0000

    Employees Module Added

--- webapp/controller/Employees.controller.js
+++ webapp/controller/Employees.controller.js
@@ -8,8 +8,16 @@ sap.ui.define([
         onInit:function(){
             var oModel=this.getOwnerComponent().getModel("Departments");
             this.getView().setModel(oModel,'Departments');
+            var modelEmp=this.getOwnerComponent().getModel("Employees");
+            this.getView().setModel(modelEmp,'Employees');
             this.oRouter=sap.ui.core.UIComponent.getRouterFor(this);
             this.oRouter.getRoute("Employees");
+        },
+        onEdit:function(oEvent){
+            
+        },
+        onDelete:function(oEvent){
+            
         }
         
     })
