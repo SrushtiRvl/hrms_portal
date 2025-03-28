2c9d8d042725e883e2c36d7b6b4eb130c02feda3
Srushti Raval <srushtiraval1205@gmail.com>
2025-03-28 13:22:04 +0000

    added signature pad

--- webapp/controller/AddEmployee.controller.js
+++ webapp/controller/AddEmployee.controller.js
@@ -10,6 +10,16 @@ sap.ui.define([
             this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
             this.oRouter.getRoute("AddEmployee");
 
+            this.oModelModule = this.getOwnerComponent().getModel()
+            this.oModelModule.read("/Modules", {
+                success: (data, res) => {
+                    debugger
+                    this.getView().setModel(this.oModelModule);
+                },
+                error: (e) => {
+                    debugger
+                }
+            })
             this.oEmpModel = this.getOwnerComponent().getModel('Employees');
             this.getView().setModel(this.oEmpModel, "Employees")
             this.oEmpModel.setProperty('/formData', {
@@ -52,44 +62,45 @@ sap.ui.define([
             if(e.sId === "liveChange"){
                 var v=e.getParameter('value');
                 e.getSource().setProperty('value',v);
-                // var data=e.getParameter('value');
-                // var property=e.get
             }
             var formData = this.oEmpModel.getProperty('/formData');
             this.wizard = this.getView().byId('idWizard');
-            var pEmail = this.getView().byId('pEmail')
-            if (pEmail.getValue()) {
-                var value = pEmail.getValue();
+            var pEmail= formData.personalEmail;
+            if (pEmail) {
+                // var value = pEmail.getValue();
                 var mailregex = /^\w+[\w-+\.]*\@\w+([-\.]\w+)*\.[a-zA-Z]{2,}$/;
-
-                if (!mailregex.test(value)) {
-                    pEmail.setValueState(sap.ui.core.ValueState.Error);
+                
+                var sid=e.getSource().sId
+                if (!mailregex.test(pEmail)) {
+                    sap.ui.getCore().byId(sid).setValueState(sap.ui.core.ValueState.Error)
+                    // pEmail.setValueState(sap.ui.core.ValueState.Error);
                 }
                 else {
-                    pEmail.setValueState(sap.ui.core.ValueState.Success);
+                    sap.ui.getCore().byId(sid).setValueState(sap.ui.core.ValueState.Success)
+                    // pEmail.setValueState(sap.ui.core.ValueState.Success);
                 }
             }
             var mobile = this.getView().byId('mobile');
-            var mData = mobile.getValue();
+            // var mData = mobile.getValue();;
+            var mData = formData.mobile;
+
             if (mData) {
+                var sid=e.getSource().sId
+                sap.ui.getCore().byId(sid).setValueState(sap.ui.core.ValueState.Error)
                 if (mData.length == 10) {
-                    mobile.setValueState(sap.ui.core.ValueState.Success);
+                    sap.ui.getCore().byId(sid).setValueState(sap.ui.core.ValueState.Success)
+                    // mobile.setValueState(sap.ui.core.ValueState.Success);
                 }
                 else {
-                    mobile.setValueState(sap.ui.core.ValueState.Error);
+                    sap.ui.getCore().byId(sid).setValueState(sap.ui.core.ValueState.Error)
+                    // mobile.setValueState(sap.ui.core.ValueState.Error);
                 }
 
             }
-
-            var fn = this.getView().byId('fName');
-            var fnData = fn.getValue();
-
-            var ln = this.getView().byId('lName');
-            var lnData = ln.getValue();
             
             
-            // if (formData.fn != '' && formData.ln != '' && formData.mobile != '' && formData.mobile.length == 10 && formData.personalEmail != '') {
-            if (fnData != '' && lnData != '' && mData != '' && mData.length == 10 && pEmail != '') {
+            if (formData.fn != '' && formData.ln != '' && formData.mobile != '' && formData.mobile.length == 10 && formData.personalEmail != '') {
+            // if (fnData != '' && lnData != '' && mData != '' && mData.length == 10 && pEmail != '') {
                 this.wizard.validateStep(this.byId('PersonalDetails'))
                 this.getView().byId('next').setVisible();
                 this.byId('PersonalDetails').setIcon('sap-icon://accept')
@@ -120,7 +131,272 @@ sap.ui.define([
                 //     this.byId('PersonalDetails').setIcon('sap-icon://account')
                 // }
             // })
+        },
+        projectDetails:function(){
+
+            this.getView().byId("html").setContent("<canvas id='signature-pad' width='400' height='200' class='signature-pad'></canvas>");
+        },
+        onSign : function(oEvent){
+            var canvas = document.getElementById("signature-pad");
+            var context = canvas.getContext("2d");
+            canvas.width = 276;
+            canvas.height = 180;
+            context.fillStyle = "#fff";
+            context.strokeStyle = "#444";
+            context.lineWidth = 1.5;
+            context.lineCap = "round";
+            context.fillRect(0, 0, canvas.width, canvas.height);
+            var disableSave = true;
+            var pixels = [];
+            var cpixels = [];
+            var xyLast = {};
+            var xyAddLast = {};
+            var calculate = false;
+            { 	//functions
+                function remove_event_listeners() {
+                    canvas.removeEventListener('mousemove', on_mousemove, false);
+                    canvas.removeEventListener('mouseup', on_mouseup, false);
+                    canvas.removeEventListener('touchmove', on_mousemove, false);
+                    canvas.removeEventListener('touchend', on_mouseup, false);
+    
+                    document.body.removeEventListener('mouseup', on_mouseup, false);
+                    document.body.removeEventListener('touchend', on_mouseup, false);
+                }
+    
+                function get_coords(e) {
+                    var x, y;
+    
+                    if (e.changedTouches && e.changedTouches[0]) {
+                        var offsety = canvas.offsetTop || 0;
+                        var offsetx = canvas.offsetLeft || 0;
+    
+                        x = e.changedTouches[0].pageX - offsetx;
+                        y = e.changedTouches[0].pageY - offsety;
+                    } else if (e.layerX || 0 == e.layerX) {
+                        x = e.layerX;
+                        y = e.layerY;
+                    } else if (e.offsetX || 0 == e.offsetX) {
+                        x = e.offsetX;
+                        y = e.offsetY;
+                    }
+    
+                    return {
+                        x : x, y : y
+                    };
+                };
+    
+                function on_mousedown(e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+    
+                    canvas.addEventListener('mouseup', on_mouseup, false);
+                    canvas.addEventListener('mousemove', on_mousemove, false);
+                    canvas.addEventListener('touchend', on_mouseup, false);
+                    canvas.addEventListener('touchmove', on_mousemove, false);
+                    document.body.addEventListener('mouseup', on_mouseup, false);
+                    document.body.addEventListener('touchend', on_mouseup, false);
+    
+                    empty = false;
+                    var xy = get_coords(e);
+                    context.beginPath();
+                    pixels.push('moveStart');
+                    context.moveTo(xy.x, xy.y);
+                    pixels.push(xy.x, xy.y);
+                    xyLast = xy;
+                };
+    
+                function on_mousemove(e, finish) {
+                    e.preventDefault();
+                    e.stopPropagation();
+    
+                    var xy = get_coords(e);
+                    var xyAdd = {
+                        x : (xyLast.x + xy.x) / 2,
+                        y : (xyLast.y + xy.y) / 2
+                    };
+    
+                    if (calculate) {
+                        var xLast = (xyAddLast.x + xyLast.x + xyAdd.x) / 3;
+                        var yLast = (xyAddLast.y + xyLast.y + xyAdd.y) / 3;
+                        pixels.push(xLast, yLast);
+                    } else {
+                        calculate = true;
+                    }
+    
+                    context.quadraticCurveTo(xyLast.x, xyLast.y, xyAdd.x, xyAdd.y);
+                    pixels.push(xyAdd.x, xyAdd.y);
+                    context.stroke();
+                    context.beginPath();
+                    context.moveTo(xyAdd.x, xyAdd.y);
+                    xyAddLast = xyAdd;
+                    xyLast = xy;
+    
+                };
+    
+                function on_mouseup(e) {
+                    remove_event_listeners();
+                    disableSave = false;
+                    context.stroke();
+                    pixels.push('e');
+                    calculate = false;
+                };
+            }
+            // canvas.addEventListener('touchstart', on_mousedown, false);
+            // canvas.addEventListener('mousedown', on_mousedown, false);
+    
+        },
+        
+        
+        /***********Download the Signature Pad********************/
+        
+        // saveButton : function(oEvent){
+        //     var canvas = document.getElementById("signature-pad");
+        //     var link = document.createElement('a');
+        //     link.href = canvas.toDataURL('image/jpeg'); 
+        //     link.download = 'sign.jpeg';
+        //     link.click(); 
+        //     var signaturePad = new SignaturePad(document.getElementById('signature-pad'), {
+        //           backgroundColor: 'rgb(74, 69, 69)',
+        //           penColor: 'rgb(0, 0, 0)'
+        //     })
+        // },
+        
+        /************Clear Signature Pad**************************/
+        
+        // clearButton : function(oEvent){
+        //     var canvas = document.getElementById("signature-pad");
+        //     var context = canvas.getContext("2d");
+        //     context.clearRect(0, 0, canvas.width, canvas.height);
+            
+        //     var signaturePad = new SignaturePad(document.getElementById('signature-pad'), {
+        //           backgroundColor: '#ffffff',
+        //           penColor: 'rgb(0, 0, 0)',
+        //           penWidth : '1'
+        //     })
+        // },
+
+
+
+
+
+
+
+
+        saveButton : function(oEvent){
+            var canvas = document.getElementById("signature-pad");
+            var link = document.createElement('a');
+            link.href = canvas.toDataURL('image/jpeg'); 
+            link.download = 'sign.jpeg';
+            link.click(); 
+            var signaturePad = new SignaturePad(document.getElementById('signature-pad'), {
+                  backgroundColor: '#ffffff',
+                  penColor: 'rgb(0, 0, 0)'
+            })
+        },
+
+        onSign: function (oEvent) {
+            this._initializeSignaturePad();
+        },
+        _initializeSignaturePad: function () {
+            var canvas = document.getElementById("signature-pad");
+            var context = canvas.getContext("2d");
+            canvas.width = 276;
+            canvas.height = 180;
+
+            // Set canvas styles
+            context.fillStyle = "#fff";
+            context.strokeStyle = "#444";
+            context.lineWidth = 1.5;
+            context.lineCap = "round";
+            context.fillRect(0, 0, canvas.width, canvas.height);
+
+            // Initialize variables
+            var pixels = [];
+            var xyLast = {};
+            var xyAddLast = {};
+            var calculate = false;
+
+            // Functions for drawing
+            var get_coords = function (e) {
+                var x, y;
+                if (e.changedTouches && e.changedTouches[0]) {
+                    x = e.changedTouches[0].pageX - canvas.offsetLeft;
+                    y = e.changedTouches[0].pageY - canvas.offsetTop;
+                } else {
+                    x = e.offsetX || e.layerX;
+                    y = e.offsetY || e.layerY;
+                }
+                return { x: x, y: y };
+            };
+
+            var on_mousedown = function (e) {
+                e.preventDefault();
+                var xy = get_coords(e);
+                context.beginPath();
+                context.moveTo(xy.x, xy.y);
+                pixels.push('moveStart', xy.x, xy.y);
+                xyLast = xy;
+
+                canvas.addEventListener('mousemove', on_mousemove, false);
+                canvas.addEventListener('mouseup', on_mouseup, false);
+                document.body.addEventListener('mouseup', on_mouseup, false);
+            };
+
+            var on_mousemove = function (e) {
+                var xy = get_coords(e);
+                var xyAdd = { x: (xyLast.x + xy.x) / 2, y: (xyLast.y + xy.y) / 2 };
+
+                if (calculate) {
+                    var xLast = (xyAddLast.x + xyLast.x + xyAdd.x) / 3;
+                    var yLast = (xyAddLast.y + xyLast.y + xyAdd.y) / 3;
+                    pixels.push(xLast, yLast);
+                } else {
+                    calculate = true;
+                }
+
+                context.quadraticCurveTo(xyLast.x, xyLast.y, xyAdd.x, xyAdd.y);
+                pixels.push(xyAdd.x, xyAdd.y);
+                context.stroke();
+                xyAddLast = xyAdd;
+                xyLast = xy;
+            };
+
+            var on_mouseup = function () {
+                canvas.removeEventListener('mousemove', on_mousemove, false);
+                document.body.removeEventListener('mouseup', on_mouseup, false);
+                context.stroke();
+                pixels.push('e');
+                calculate = false;
+            };
+
+            // Attach event listeners for touch and mouse interaction
+            canvas.addEventListener('mousedown', on_mousedown, false);
+            canvas.addEventListener('touchstart', on_mousedown, false);
+        },
+
+        saveButton: function () {
+            var canvas = document.getElementById("signature-pad");
+            if (canvas) {
+                var link = document.createElement('a');
+                link.href = canvas.toDataURL('image/jpeg');
+                link.download = 'sign.jpeg';
+                link.click();
+                MessageToast.show("Signature saved as JPEG.");
+            }
+        },
+
+        clearButton: function () {
+            var canvas = document.getElementById("signature-pad");
+            if (canvas) {
+                var context = canvas.getContext("2d");
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                MessageToast.show("Signature pad cleared.");
+            }
         }
+    
+
+
+
     })
 }
 )
--- webapp/controller/Modules.controller.js
+++ webapp/controller/Modules.controller.js
@@ -3,35 +3,135 @@ sap.ui.define([
     "sap/m/MessageToast",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator"
-], (Controller,MessageToast,Filter,FilterOperator) => {
+], (Controller, MessageToast, Filter, FilterOperator) => {
     "use strict";
 
     return Controller.extend("zhrmsportal.controller.Modules", {
-        onInit:function(){
-            this.oRouter=sap.ui.core.UIComponent.getRouterFor(this);
+        onInit: function () {
+            this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
             this.oRouter.getRoute("Modules");
 
-            var oModel=this.getOwnerComponent().getModel("Modules");
-            this.getView().setModel(oModel,'Modules');
+            this.oFormDataModel = this.getOwnerComponent().getModel('FormData')
+            this.oModelModule = this.getOwnerComponent().getModel()
+
+            this.readData()
+
+
+            // var oModel=this.getOwnerComponent().getModel("Modules");
+            // this.getView().setModel(oModel,'Modules');
         },
-        onSearch:function(oEvent){
-            var tbl=this.getView().byId('mTable');
-            var aFilter=[];
+        readData: function (e) {
+            this.oModelModule.read("/Modules", {
+                success: (data, res) => {
+                    debugger
+                    this.getView().setModel(this.oModelModule);
+                    var mt = new Set();
+                    data.results.forEach(module => {
+                        mt.add(module.MODULE_TYPE)
+                    });
+
+                    var ModuleTypes = []
+                    mt.forEach((val) => {
+                        var obj = {}
+                        obj.mKey = val.substring(0, 3)
+                        obj.mValue = val
+                        ModuleTypes.push(obj)
+                    })
+                    this.oFormDataModel.setProperty("/ModuleData", {
+                        "MODULE_CODE": "",
+                        "MODULE_TYPE": "",
+                        "MODULE_NAME": ""
+                    })
+                    this.oFormDataModel.setProperty("/ModuleTypes", ModuleTypes)
+                },
+                error: (e) => {
+                    debugger
+                }
+            })
+        },
+        onAdd: function (e) {
+            if (!this.frag) {
+                this.frag = sap.ui.xmlfragment("zhrmsportal.fragments.AddModule", this)
+                this.getView().addDependent(this.frag);
+            }
+            this.frag.open();
+        },
+        onSearch: function (oEvent) {
+            var tbl = this.getView().byId('mTable');
+            var aFilter = [];
             // var sQuery=oEvent.getParameter('query');
-            var sQuery=oEvent.getSource().getValue();
+            var sQuery = oEvent.getSource().getValue();
             aFilter.push(new Filter({
-                'filters':[
-                    new Filter('mName',FilterOperator.Contains,sQuery),
-                    new Filter('mCode',FilterOperator.EQ,sQuery),
-                    new Filter('mType',FilterOperator.Contains,sQuery)
+                'filters': [
+                    new Filter({ path: 'MODULE_NAME', caseSensitive: false, operator: FilterOperator.Contains, value1: sQuery }),
+                    new Filter({ path: 'MODULE_CODE', caseSensitive: false, operator: FilterOperator.Contains, value1: sQuery }),
+                    new Filter({ path: 'MODULE_TYPE', caseSensitive: false, operator: FilterOperator.Contains, value1: sQuery })
                 ],
-                or:true
+                or: true
             }))
             tbl.getBinding('items').filter(aFilter);
         },
-        onPress:function(oEvent){
-            var tile=oEvent.getSource().getProperty('header');
+        onPress: function (oEvent) {
+            var tile = oEvent.getSource().getProperty('header');
             this.oRouter.navTo(tile);
+        },
+        onDelete:function(e){
+            var sPath=e.getSource().getBindingContext().sPath;
+            this.oModelModule.remove(sPath,{
+                success:(e)=>{
+                    this.readData()
+                },
+                error:(e)=>{
+                    debugger
+                }
+            })
+        },
+        onAddModule: function (e) {
+            var oData=this.oFormDataModel.getProperty("/ModuleData");
+            var fields = ['CODE', 'NAME', 'TYPE']
+            var status = "OK"
+            fields.forEach((val, index) => {
+                var field = sap.ui.getCore().byId(val);
+                var value = field.getValue();
+                field.setValueState(sap.ui.core.ValueState.Error)
+                if (value !== "") {
+                    field.setValueState(sap.ui.core.ValueState.Success)
+                    fields[index] = "OK"
+                }
+            })
+            fields.forEach(val => {
+                if (val !== "OK") {
+                    status = "ERROR"
+                }
+            })
+            if (status === "OK") {
+                oData.MODULE_TYPE=sap.ui.getCore().byId("TYPE").getValue()
+                delete oData.selValue
+                this.oModelModule.create("/Modules", oData, {
+                    success: (e) => {
+                        this.readData();
+                        this.frag.close()
+                        this.frag.destroy()
+                        this.frag = null
+                    }
+                })
+            }
+            // var oData=this.oFormDataModel.getProperty("/ModuleData");
+            // var selV=oData.selValue
+            // var oComboboxData=this.oFormDataModel.getProperty("/ModuleTypes");
+            // oComboboxData.forEach((val)=>{
+            //     if(val.mKey===selV){
+            //         oData.MODULE_TYPE=val.mValue;
+            //         delete oData.selValue
+            //     }
+            // })
+
+        },
+        onAddCancelModule: function (e) {
+            this.readData();
+            this.frag.close()
+            this.frag.destroy()
+            this.frag = null
         }
     })
 }
